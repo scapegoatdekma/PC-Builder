e3342ac7cd6f0d79e75458ccc50f1a3ccb4c6e8f
dekmaOFF <safonovkirill112@gmail.com>
2026-03-26 00:40:27 +0300

    Initial Commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,17 @@
 import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import { TypographyH1 } from "@/components/ui/typography-h1";
+import { TypographyH3 } from "@/components/ui/typography-h3";
 
 export default function Home() {
   return (
     <div className="">
-      <main>Hello World</main>
+      <main>
+        <TypographyH1>Hello World</TypographyH1>
+        <TypographyH3>Hello World</TypographyH3>
+
+        <Button variant="default">Button</Button>
+      </main>
     </div>
   );
 }
